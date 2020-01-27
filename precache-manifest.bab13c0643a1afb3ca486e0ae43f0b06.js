ecafbc1d7b41bf54f6b8f3164064d73d9a638bc2
hamdi lachaal <48292562+hklachaal@users.noreply.github.com>
2020-01-26 20:25:45 -0800

    Updates

---
+++ precache-manifest.bab13c0643a1afb3ca486e0ae43f0b06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d2b5ed514331c71787c73a40f1ff99",
+    "url": "/concentration-card-game/index.html"
+  },
+  {
+    "revision": "693f675671ee41d7d7cc",
+    "url": "/concentration-card-game/static/css/main.e19b2197.chunk.css"
+  },
+  {
+    "revision": "184a0b6fc5410886f482",
+    "url": "/concentration-card-game/static/js/2.4c41e1d7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/concentration-card-game/static/js/2.4c41e1d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "693f675671ee41d7d7cc",
+    "url": "/concentration-card-game/static/js/main.fd3628f5.chunk.js"
+  },
+  {
+    "revision": "82a39f6f200ccc974322",
+    "url": "/concentration-card-game/static/js/runtime-main.c7467bb8.js"
+  },
+  {
+    "revision": "9eb3c0cfc68e51df5e6c8d8f31feee75",
+    "url": "/concentration-card-game/static/media/card-back.9eb3c0cf.svg"
+  }
+]);
